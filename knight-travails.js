fa3db699f06e6a61757b3350a4028111dbd3ed4c
Justice <122542069+goob8079@users.noreply.github.com>
2025-09-14 15:01:52 +0900

    added some features

--- knight-travails.js
+++ knight-travails.js
@@ -1,13 +1,27 @@
-const current = [];
-const queue = []; // keep track of next postitions
-const visited = []; 
-
 function knightMoves(start, end) {
     if (start.length < 2 || end.length < 2) throw new Error('Coordinate must be in format [x, y]');
     
-    if (start[0] < 0 || start[1] > 7) throw new Error('Start coordinate out of bounds!');
+    const isValid = ([x, y]) => x >= 0 && x <= 7 && y >= 0 && y <= 7;
+    
+    if (!isValid([start[0], start[1]])) throw new Error('Start coordinate out of bounds!');
+    
+    if (!isValid([end[0], end[1]])) throw new Error('End coordinate out of bounds!');
+    
+    if (start[0] === end[0] && start[1] === end[1]) {
+        return [start];
+    }
+    
+    let moves = [
+        [2, 1], [2, -1], [-2, 1], [-2, -1],
+        [1, 2], [1, -2], [-1, 1], [-1, -2]
+    ];
 
-    if (end[0] < 0 || end[1] > 7) throw new Error('End coordinate out of bounds!');
+    let queue = [start]; // keep track of next postitions
+    let visited = new Set([start.toString()]);
+
+    while (queue.length > 0) {
+        
+    }
 
     
 }
